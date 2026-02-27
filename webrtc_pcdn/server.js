289faba5287cd065b115d269c2a0a2e66c483a1a
linqibin <linqibin@agora.io>
2026-02-27 15:54:57 +0800

    x

--- webrtc_pcdn/server.js
+++ webrtc_pcdn/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: '*' }, serveClient: false });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
